fde929673ac3cf8d3b4f02e3f442a0f2824050b2
iarmoghan <iarmoghan@gmail.com>
2021-12-18 16:44:46 +0000

    Adding jsdoc style comment for gallery toggle function

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -1,10 +1,22 @@
-function showMsg(obj) {
-var inner1 = document.getElementById("msg");
-let message = obj.getAttribute("msg");
-inner1.innerHTML = message;
-if (inner1.style.display == "none") {
-inner1.style.display = "";
-} else {
-inner1.style.display = "none";
-}
+/**
+ * Function to toggle and set message
+ * for selected image in gallery view.
+ * @param {object} selected_image - The selected image.
+ * @returns {undefined}
+ */
+function showMsg(selected_image) {
+    // finding message container
+    const message_box = document.getElementById("msg");
+    // extracting message from selected image
+    const message = selected_image.getAttribute("msg");
+    // setting message for the selected image
+    message_box.innerHTML = message;
+    // checking if image is hidden
+    if (message_box.style.display == "none") {
+        // making message box visible be setting empty display style
+        message_box.style.display = "";
+    } else {
+        // hiding message box by making display style none.
+        message_box.style.display = "none";
+    }
 }
